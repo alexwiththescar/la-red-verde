de7f30c2077c942ed86cb59480a50dde35dba84c
Alexwiththescar <thesalesstar@gmail.com>
2013-05-09 00:06:31 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TgnAgain::Application.config.secret_token = '3ebc4a007b3c1eb61d8d30461500d5555775082502343a65a3eac5a8f37a5b23cb85e9bca2aa1588f8d06260c83b46ce666c5e47ac5fe38d7422beb8b63663e5'
