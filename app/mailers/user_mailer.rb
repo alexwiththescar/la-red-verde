a758c14f471df98f39da29d04b40f123a40c0a81
Alexwiththescar <thesalesstar@gmail.com>
2013-06-21 11:25:37 +0200

    Finally sending the email bodys!

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,8 @@ class UserMailer < ActionMailer::Base
  def welcome_email(user)
     @user = user
     @url  = "http://example.com/login"
+    @message = message
+    @sender = message.sender
     mail(:to => user.email, :subject => "Welcome to My Awesome Site")
   end
 end
-
