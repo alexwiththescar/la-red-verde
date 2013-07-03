e793a441c13e04f71763679af1f305c268b31089
Alexwiththescar <thesalesstar@gmail.com>
2013-07-03 17:21:50 +0200

    removed gmappable from users because of error and not needed!

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User < ActiveRecord::Base
   
   geocoded_by :address
   after_validation :geocode, :if => :region_changed?
-acts_as_gmappable
+
 
 def gmaps4rails_address
 #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
